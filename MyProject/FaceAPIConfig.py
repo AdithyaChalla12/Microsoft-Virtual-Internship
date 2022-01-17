589e0bc6ef34ec1f141dce7c914d459915c8ddbb
C.S.V Adithya <70251229+AdithyaChalla12@users.noreply.github.com>
2022-01-18 00:46:23 +0530

    Delete FaceAPIConfig.py

--- MyProject/FaceAPIConfig.py
+++
@@ -1,8 +0,0 @@
-
-subscription_key = "4f9266c5bf4c41bbaa750ad4542ccc46 " 
-face_api_url = 'https://my-1-faceapi.cognitiveservices.azure.com/detect'
-
-
-def config():
-    print("Call Config")
-    return subscription_key, face_api_url
